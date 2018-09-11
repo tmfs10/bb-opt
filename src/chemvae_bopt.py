722182e9bfd34fdfadca45e9682be01d18353ec6
Siddhartha Jain <sj1@oct1.csail.mit.edu>
2018-09-11 15:09:57 -0400

    added gpopt_pes for chemvae; not debugged

--- src/chemvae_bopt.py
+++ src/chemvae_bopt.py
@@ -4,9 +4,14 @@ import torch.nn as nn
 import matlab.engine
 import acquisition as ack
 
-def gpopt_pes(x_min, x_max, predictor_model):
+def gpopt_pes(n_init_samples, n_opt_samples, n_features, x_min, x_max, predictor_model, num_batches):
     eng = matlab.engine.start_matlab()
 
-    f
+    x_samples, y_samples, l, sigma, sigma0 = ack.init_pes(eng, predictor_model, n_init_samples, n_opt_samples, x_min, x_max)
+
+    for i in range(num_batches):
+        x_samples, y_samples, guesses, l, sigma, sigma0 = ack.pes(eng, obj, n_opt_samples, n_features, x_min, x_max, x_samples, guesses, y_samples, l, sigma, sigma0)
 
     eng.quit()
+
+    return np.asarray(x_samples), np.asarray(y_samples), np.asarray(guesses)
